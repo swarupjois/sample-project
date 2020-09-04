88656f0cba0617f9a270dfa7304a1f4da91f4b71
swarupjois <34804439+swarupjois@users.noreply.github.com>
2020-09-05 00:28:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
-#building client
+#building client.
 cd sample-project-client
 npm install
 npm run build
 
-#building server
+#building server.
 cd ../sample-project-server
 npm install
 
-#bundling client to server
+#bundling client to server.
 mkdir -p public
 cp -r ../sample-project-client/build public
 
