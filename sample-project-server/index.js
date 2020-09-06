7dc021908229caa4c4483f1b9c2a527380d2b1ac
swarupjois <34804439+swarupjois@users.noreply.github.com>
2020-09-06 16:59:47 +0530

    bug fixes

--- sample-project-server/index.js
+++ sample-project-server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
 const app = express()
-console.log("myprofile:", __dirname);
-app.use('/myprofile',express.static("C:/Users/SRISAPURA/Documents/SampleProject1/sample-project-server/public/build"));
+app.use('/myprofile',express.static(__dirname + "/public/build"));
 app.listen(8081);
